41e445b8f199c2081e39596971abf639d7420bbf
JIELSAN <jesussjg811@gmail.com>
2024-11-23 15:37:46 -0600

    creacion de las funciones de users

---
+++ src/controllers/userController.js
@@ -0,0 +1,15 @@
+//Importacion de la conecciona la bd
+const connection = require('../models/dbConnection');
+
+//Funciones
+exports.register = async (req, res) => {
+
+}
+
+exports.login = async (req, res) => {
+
+}
+
+exports.update = async (req, res) => {
+
+}

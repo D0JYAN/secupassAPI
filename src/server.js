9f3ad171956078f782060d74f17962cf58392b09
JIELSAN <jesussjg811@gmail.com>
2024-11-23 15:37:32 -0600

    importacion de las rutas

--- src/server.js
+++ src/server.js
@@ -7,14 +7,19 @@ const cors = require('cors');
 
 //Crear una instancia de la aplicación
 const app = express();
-// Configurar CORS para permitir conexiones desde cualquier origen
+//Configurar CORS para permitir conexiones desde cualquier origen
 app.use(cors());
-// Configurar el formato de los datos a recibir
+//Configurar el formato de los datos a recibir
 app.use(bodyParser.json());
 
-//asignar el puerto
-const PORT = process.env.PORT ?? 3000;
+//Importacion de las rutas
+const userRoute = require('./routes/userRoute');
 
+//Rutas base
+app.use('/user', userRoute);
+
+//Asignar el puerto
+const PORT = process.env.PORT ?? 3000;
 //Levantar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor Activo En El Puerto: ${PORT}`);
